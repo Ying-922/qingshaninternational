62f4ab8e09935a308a5b365dc8c24d7acd460737
Ying-922 <wcying.922@gmail.com>
2025-01-17 08:22:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// 讓導航連結平滑滾動到對應的區塊
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
